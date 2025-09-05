213244f7e8b87c4ef501e4394dfb71ee5dadb612
Nikita Lugachev <nikitalugachev149th@gmail.com>
2025-09-05 19:19:37 +0700

    Initial Spring Boot project setup with PostgreSQL

---
+++ src/main/java/com/tsu/tsueventhubapi/TsuEventhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.tsu.tsueventhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsuEventhubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsuEventhubApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/tsu/tsueventhubapi/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.tsu.tsueventhubapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TSU EventHub API")
+                        .version("1.0")
+                        .description("Документация REST API для проекта TSU EventHub"));
+    }
+}
