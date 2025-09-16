935cc47db1e59998f5dd2b508de77ffa31da55db
Nikita Lugachev <nikitalugachev149th@gmail.com>
2025-09-16 14:36:07 +0700

    Add production server to OpenAPI configuration

--- src/main/java/com/tsu/tsueventhubapi/config/OpenApiConfig.java
+++ src/main/java/com/tsu/tsueventhubapi/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.tsu.tsueventhubapi.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,9 @@ public class OpenApiConfig {
                 .info(new Info().title("TSU EventHub API").version("1.0").description(
                         "Документация REST API для проекта TSU EventHub"
                 ))
+                .addServersItem(new Server()
+                        .url("https://api.tsu-eventhub.orexi4.ru:8443")
+                        .description("Production server with HTTPS"))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes(securitySchemeName,
                                 new SecurityScheme()
